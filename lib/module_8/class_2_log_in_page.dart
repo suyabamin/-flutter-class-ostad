b362ec6a13d8d2599db5a1681032b599c219b81c
suyab-amin <suyabamins@gmail.com>
2025-11-14 18:26:08 +0600

    added dinamic log in and passord page

--- lib/module_8/class_2_log_in_page.dart
+++ lib/module_8/class_2_log_in_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_13_calss/home.dart';
+import 'package:flutter_13_calss/module_8/dashboard.dart';
+import 'package:flutter_13_calss/module_8/home_2.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+    final _formKey=GlobalKey<FormState>();
+    TextEditingController phoneController=TextEditingController();
+    TextEditingController passwordController=TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: Text('Log in page'),
@@ -20,22 +27,31 @@ class LoginPage extends StatelessWidget {
             height: 20,
           ),
           Form(
+            key: _formKey,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
                   Text('phone number'),
                   TextFormField(
+                    controller: phoneController,
                     keyboardType: TextInputType.phone,
                     decoration: InputDecoration(
                       hintText: 'phone number',
-                          border: OutlineInputBorder(
-
-                          ),
-                    ),
+                          border: OutlineInputBorder(),),
+                    validator: (value){
+                      if(value==null||value.isEmpty){
+                        return 'Please enter phone number';
+                      }else if(value.length!=11){
+                        return 'pelesr enter correct phone number';
+                      }else{
+                        return null;
+                      }
+                    },
                   ),
                    SizedBox(height: 25,),
-                  TextField(
+                  TextFormField(
+                    controller: passwordController,
                     obscureText: true,
                     decoration: InputDecoration(
                       hintText: 'Password',
@@ -43,7 +59,56 @@ class LoginPage extends StatelessWidget {
 
                       ),
                     ),
+                    validator: (value){
+                      if(value==null||value.isEmpty){
+                        return 'please enter passwodrd';
+                      }else if(value.length<=6){
+                        return 'password must be at least 6 characters';
+                      }else{
+                        return null;
+                      }
+                    },
                   ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: ElevatedButton(
+                        onPressed: (){
+                          if(_formKey.currentState!.validate()){   ///if all condition is true this button tap will work properly
+                            Navigator.push(
+                                context,
+                            MaterialPageRoute(
+                                builder:(context)=>Home()));   ///pusg enother page one page to another page jump
+
+                            // Navigator.pushReplacement(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) => Home()));
+                          }
+                        },
+                        child: Text('submit')),
+
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed:(){
+                    Navigator.push(context,
+                    MaterialPageRoute(builder:(context)=>Home2()));
+                  }, child:Text('home 2')),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: (){
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context)=>Dashboard(phone: phoneController.text)));
+                  },
+                      child: Text('Dashboard'))
                 ],
               ),
             ),
